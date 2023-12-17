4ab134b49ea26f4bee6c85a026d1e716cbe439d3
Zaituny <mohamedinv.580@gmail.com>
2023-12-18 00:15:53 +0200

    testing CI for maven

---
+++ src/main/java/com/nuggets/IP/AsutApplication.java
@@ -0,0 +1,13 @@
+package com.nuggets.IP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nuggets/IP/AsutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuggets.IP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
