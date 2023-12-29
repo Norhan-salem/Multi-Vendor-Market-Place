f8bc96f3763f815037ea17ae57a117324f5deec5
Zaituny <mohamedinv.580@gmail.com>
2023-12-29 22:17:57 +0200

    added implementation for seller registration function in SellerServiceImpl

--- src/main/java/com/nuggets/IP/service/impl/SellerServiceImpl.java
+++ src/main/java/com/nuggets/IP/service/impl/SellerServiceImpl.java
@@ -2,7 +2,11 @@ package com.nuggets.IP.service.impl;
 
 import com.nuggets.IP.exception.AppUserAlreadyExistsException;
 import com.nuggets.IP.exception.SellerAlreadyExistsException;
+import com.nuggets.IP.model.AppUser;
 import com.nuggets.IP.model.Seller;
+import com.nuggets.IP.model.repository.AppUserRepository;
+import com.nuggets.IP.service.EncryptionService;
+import com.nuggets.IP.service.JWTService;
 import com.nuggets.IP.service.SellerService;
 import com.nuggets.IP.web.rest.request.SellerRegistrationBody;
 import org.springframework.stereotype.Service;
@@ -10,8 +14,32 @@ import org.springframework.stereotype.Service;
 @Service
 public class SellerServiceImpl implements SellerService {
 
+    private final AppUserRepository appUserRepository;
+
+    private final EncryptionService encryptionService;
+
+    private JWTService jwtService;
+    public SellerServiceImpl(AppUserRepository appUserRepository, EncryptionServiceImpl encryptionService) {
+        this.appUserRepository = appUserRepository;
+        this.encryptionService = encryptionService;
+    }
+
     @Override
     public Seller register(SellerRegistrationBody registrationBody) throws SellerAlreadyExistsException {
-        return null;
+        if (appUserRepository.findByUsernameIgnoreCase(registrationBody.getUsername()).isPresent() ||
+                appUserRepository.findByEmailIgnoreCase(registrationBody.getEmail()).isPresent()) {
+            throw new SellerAlreadyExistsException("User already exists");
+        }
+        Seller seller = new Seller();
+        seller.setUsername(registrationBody.getUsername());
+        seller.setPassword(encryptionService.encryptPassword(registrationBody.getPassword()));
+        seller.setEmail(registrationBody.getEmail());
+        seller.setFirstName(registrationBody.getFirstName());
+        seller.setLastName(registrationBody.getLastName());
+        seller.setPhoneNumber(registrationBody.getPhoneNumber());
+        seller.setLocation(registrationBody.getLocation());
+        seller.setNeighborhood(registrationBody.getNeighborhood());
+        
+        return appUserRepository.save(seller);
     }
 }
