3b6ccd47040e80ef8f338601e44a395f3adec6b6
Zaituny <mohamedinv.580@gmail.com>
2023-12-29 17:36:16 +0200

    added local DB

--- src/main/java/com/nuggets/IP/AsutApplication.java
+++ src/main/java/com/nuggets/IP/AsutApplication.java
@@ -2,12 +2,16 @@ package com.nuggets.IP;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import io.github.cdimascio.dotenv.Dotenv;
 @SpringBootApplication
 public class AsutApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AsutApplication.class, args);
-	}
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+		// Set the environment variables
+		System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
+		System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
+		SpringApplication.run(AsutApplication.class, args);}
 
 }
--- src/test/java/com/nuggets/IP/AsutApplicationTests.java
+++ src/test/java/com/nuggets/IP/AsutApplicationTests.java
@@ -1,11 +1,21 @@
 package com.nuggets.IP;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootTest
 class AsutApplicationTests {
+	@BeforeAll
+	public static void loadEnvVariables() {
+		// Load environment variables from .env
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
 
+		// Set the environment variables as system properties
+		System.setProperty("db_username", dotenv.get("DB_USERNAME"));
+		System.setProperty("db_password", dotenv.get("DB_PASSWORD"));
+	}
 	@Test
 	void contextLoads() {
 	}
