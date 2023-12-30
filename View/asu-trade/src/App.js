eaf96346e031be03b44c29be14cf64e17380f1d7
Mina <minassgerges@gmail.com>
2023-12-31 01:44:18 +0200

    temp

--- View/asu-trade/src/App.js
+++ View/asu-trade/src/App.js
@@ -12,6 +12,7 @@ import Search from "./Pages/Search/Search";
 import NoPage from "./Pages/NoPage/NoPage";
 import TermsAndConditions from "./Pages/TermsAndConditions/TermsAndConditions";
 import HomePage from "./Pages/HomePage/HomePage";
+import ProductPage from "./Pages/ProductPage/ProductPage";
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
@@ -21,8 +22,12 @@ const router = createBrowserRouter(
 			<Route path="/Login" element={<Login />} />
 			<Route path="/Search" element={<Search />} />
 			<Route path="/SellerSignup" element={<Seller_Signup />} />
-			<Route path="/TermsAndConditions" element={<TermsAndConditions />}/>
+			<Route
+				path="/TermsAndConditions"
+				element={<TermsAndConditions />}
+			/>
 			<Route path="/Home" element={<HomePage />} />
+			<Route path="/ProductPage" element={<ProductPage />} />
 			<Route path="/*" element={<NoPage />} />
 		</Route>
 	)
---
+++ View/asu-trade/src/Components/ProductSellerAside/ProductSellerAside.jsx
@@ -0,0 +1,3 @@
+export default function ProductSellerAside({ product }) {
+	return <aside>seller aside</aside>;
+}
