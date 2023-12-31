9209c8775bdd0788586c9309a1e785f1754ee0b3
Zaituny <mohamedinv.580@gmail.com>
2023-12-31 05:24:24 +0200

    added more features

--- src/main/java/com/nuggets/IP/service/WishItemService.java
+++ src/main/java/com/nuggets/IP/service/WishItemService.java
@@ -1,4 +1,14 @@
 package com.nuggets.IP.service;
 
+import com.nuggets.IP.exception.AppUserDoesNotExistException;
+import com.nuggets.IP.model.Product;
+import com.nuggets.IP.model.WishItem;
+import lombok.With;
+
+import java.util.List;
+
 public interface WishItemService {
+    void addWishItemToUser(Long userId, Long ProductId);
+    void removeWishItemFromUser(String username, Long ProductId) throws AppUserDoesNotExistException;
+    List<WishItem> getWishItemsByUserId(Long userId);
 }
--- src/main/java/com/nuggets/IP/service/impl/WishItemServiceImpl.java
+++ src/main/java/com/nuggets/IP/service/impl/WishItemServiceImpl.java
@@ -1,8 +1,41 @@
 package com.nuggets.IP.service.impl;
 
+import com.nuggets.IP.exception.AppUserDoesNotExistException;
+import com.nuggets.IP.model.AppUser;
+import com.nuggets.IP.model.WishItem;
+import com.nuggets.IP.model.repository.AppUserRepository;
+import com.nuggets.IP.model.repository.WishItemRepository;
 import com.nuggets.IP.service.WishItemService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class WishItemServiceImpl implements WishItemService {
+
+    private final WishItemRepository wishItemRepository;
+    private final AppUserRepository appUserRepository;
+
+    public WishItemServiceImpl(WishItemRepository wishItemRepository,
+                               AppUserRepository appUserRepository) {
+        this.wishItemRepository = wishItemRepository;
+        this.appUserRepository = appUserRepository;
+    }
+
+    @Override
+    public void addWishItemToUser(Long userId, Long ProductId) {
+        // TODO: implement wishlist adding logic
+    }
+
+    @Override
+    public void removeWishItemFromUser(String username, Long ProductId) throws AppUserDoesNotExistException{
+        AppUser appUser = appUserRepository.findByUsernameIgnoreCase(username)
+                .orElseThrow(() -> new AppUserDoesNotExistException("User not found"));
+        wishItemRepository.deleteByAppUsers(appUser);
+    }
+
+    @Override
+    public List<WishItem> getWishItemsByUserId(Long userId) {
+        return wishItemRepository.findByAppUsers_UserID(userId);
+    }
 }
